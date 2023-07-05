7816551eb6acdc54c408cf2027ba41a5902bc320
Shubha Dhang <shubhadhang@gmail.com>
2023-07-05 12:38:13 +0200

    Used class and switch case

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,58 @@ package org.example;
 
 import java.util.Scanner;
 
+class Addition
+        {
+            double add;
+            void Add(double n1, double n2)
+            {
+                add=n1+n2;
+                System.out.println("Addition of two numbers:" +add);
+
+            }
+
+        }
+        class Subtraction{
+
+                double sub;
+            public void Sub(double n1, double n2) {
+                System.out.println("Subtraction of two numbers:");
+                sub = n1 - n2;
+                System.out.println("Subtraction of two numbers:" +sub);
+            }
+        }
+        class Multiplication{
+
+                double mul;
+            public void  Mul(double n1, double n2) {
+                System.out.println("MULTIPLICATION");
+                mul = n1 * n2;
+                System.out.println("Multiplication of two numbers:" +mul);
+
+            }
+        }
+        class Division{
+
+                double div;
+            public void Div(double n1, double n2) {
+                try {
+                     div = n1 / n2;
+                    //System.out.println("Division of two numbers:" + div);
+                }
+                catch (ArithmeticException ae)
+                {
+                    System.out.println("ArithmeticException--"+ae.getMessage());
+                }
+                System.out.println("Division of two numbers:"+div);
+            }
+        }
 public class Main {
-    
+
     public static void main(String[] args) {
 
 
         double n1, n2;
-        float f1, f2;
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hi welcome to simple calculator....!!");
         boolean y = true;
@@ -21,65 +66,62 @@ public class Main {
             System.out.println("4-DIVISION");
             System.out.println("5-exit");
             System.out.println("-----------------------------------------------------------------------");
+
             int num = scanner.nextInt();
+
             switch (num) {
                 case 1: {
-                    System.out.println("---ADDITION---");
                     System.out.println("enter first number:");
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                    System.out.println("Addition");
-                    double add = n1 + n2;
-                    System.out.println("Addition of two numbers:" + add);
+                    System.out.println("---ADDITION---");
+                    Addition ad=new Addition();
+                    ad.Add(n1,n2);
                     break;
                 }
                 case 2: {
-                    System.out.println("---SUBTRACTION---");
                     System.out.println("enter first number:");
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                    System.out.println("Subtraction of two numbers:");
-                    double sub = n1 - n2;
-                    System.out.println("Subtraction of two numbers:" + sub);
+                    System.out.println("---SUBTRACTION---");
+                    Subtraction sb=new Subtraction();
+                    sb.Sub(n1,n2);
                     break;
 
                 }
                 case 3: {
-                    System.out.println("---MULTIPLICATION---");
                     System.out.println("enter first number:");
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
+                    System.out.println("---MULTIPLICATION---");
+                    Multiplication ml=new Multiplication();
+                    ml.Mul(n1,n2);
 
-                    double mul = n1 * n2;
-                    System.out.println("Multiplication of two numbers:" + mul);
                     break;
                 }
                 case 4: {
-                    System.out.println("---DIVISION---");
                     System.out.println("enter first number:");
                     n1 = scanner.nextInt();
                     System.out.println("enter second number:");
                     n2 = scanner.nextInt();
-                    try {
-                        double div = n1 / n2;
-                        System.out.println("Division of two numbers:" + div);
-                    }
-                    catch (ArithmeticException ae)
-                    {
-                        System.out.println("ArithmeticException--"+ae.getMessage());
-                    }
+                    System.out.println("---DIVISION---");
+                    Division di= new Division();
+                    di.Div(n1,n2);
+
+
+
                     break;
 
                 }
                 case 5:
                 {
                     y=false;
-                    break;}
+                    break;
+                }
                 default:
-
                     System.out.println("you have choose the wrong option");
 
 
